be13e12f7d03e40fb700e646f187bd0080c204c9
Stephen Gruzin <stephengrzn@gmail.com>
2023-03-08 19:35:14 -0800

    Reformatted the slideup component

--- components/SlideUp.tsx
+++ components/SlideUp.tsx
@@ -13,13 +13,18 @@ export default function SlideUp({ header, children, close, border, closeInternal
     const win = Dimensions.get('window');
     const fadeAnim = useRef(new Animated.Value(0)).current;
 
-    useEffect(() => {
+
+    function changeTiming(toValue: number) {
         Animated.timing(fadeAnim, {
-            toValue: -win.height+90,
+            toValue,
             duration: 300,
             easing: Easing.linear,
             useNativeDriver: Platform.OS==="web" ? false : true
-          }).start();
+        }).start();
+    }
+
+    useEffect(() => {
+        changeTiming(-win.height+90);
     }, []);
 
     useEffect(() => {
@@ -29,41 +34,40 @@ export default function SlideUp({ header, children, close, border, closeInternal
     }, [closeInternal]);
 
     function closeIt() {
-        Animated.timing(fadeAnim, {
-            toValue: 0,
-            duration: 300,
-            useNativeDriver: Platform.OS === "web" ? false : true
-          }).start();
+        changeTiming(0);
         setTimeout(close, 300);
     }
+  return (
+    <Animated.View style={[styles.main, {
+        borderColor: border === "red" ? "#EE3F3f" : border === "blue" ? "#3A9FE9" : border === "yellow" ? "#ffbc00" : "#000000",
+        height: win.height - 90,
+        top: win.height-70,
+        transform: [{translateY: fadeAnim}]
+      }]}>
+        <View style={styles.header}>
+            <Text style={styles.headerTitle} numberOfLines={1}>{header}</Text>
+            <TouchableOpacity style={styles.closeButton} onPress={closeIt}>
+              <Image style={styles.image} source={require('../assets/closecircle.png')} />
+            </TouchableOpacity>
+        </View>
+        <View style={[styles.main2, {height: win.height - 140}]}>
+            {children}
+        </View>
+    </Animated.View>
+  )
+}
 
-    useEffect(() => {
-        console.log("the border changed")
-    }, [border]);
-
-    // style={{ top, border: `2px solid ${borderColor}` }}
 const styles = StyleSheet.create({
-    cover: {
-        width: win.width,
-        height: win.height - 70,
-        position: 'relative',
-        zIndex: 100,
-        top: 0,
-        left: 0
-    },
     main: {
         width: "100%",
-        height: win.height - 90,
         backgroundColor: "#f3f3f3",
         borderTopLeftRadius: 35,
         borderTopRightRadius: 35,
         borderWidth: 2,
         borderStyle: "solid",
-        borderColor: border === "red" ? "#EE3F3f" : border === "blue" ? "#3A9FE9" : border === "yellow" ? "#ffbc00" : "#000000",
         borderBottomWidth: 0,
         position: "absolute",
         left: 0,
-        top: win.height-70,
         zIndex: 105
     },
     header: {
@@ -88,7 +92,6 @@ const styles = StyleSheet.create({
     },
     main2: {
         width: "100%",
-        height: win.height - 140,
         paddingBottom: 10
     },
     image: {
@@ -96,20 +99,3 @@ const styles = StyleSheet.create({
         height: 20
     }
 });
-  return (
-    <Animated.View style={[styles.main, {
-        // Bind opacity to animated value
-        transform: [{translateY: fadeAnim}]
-      }]}>
-        <View style={styles.header}>
-            <Text style={styles.headerTitle} numberOfLines={1}>{header}</Text>
-            <TouchableOpacity style={styles.closeButton} onPress={closeIt}>
-              <Image style={styles.image} source={require('../assets/closecircle.png')} />
-            </TouchableOpacity>
-        </View>
-        <View style={styles.main2}>
-            {children}
-        </View>
-    </Animated.View>
-  )
-}
